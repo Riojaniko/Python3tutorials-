d5edfd65f028f97f016d9f154020d6f5cfbab107
Riojaniko <128044247+Riojaniko@users.noreply.github.com>
2023-03-21 15:13:14 +0100

    Add files via upload

---
+++ Listas/01-Listas.py
@@ -0,0 +1,37 @@
+"""Las listas son usadas para almacenar articulos. Estas son creadas usando [] corchetes con comas separando los elementos"""
+
+listnums = ["uno", "dos", "tres"]
+intnums = [1,2,3,4,5,6]
+print(listnums)
+print(intnums)
+
+"""si queremos acceder a un determinado elemento tenemos que usar su indice en corchetes"""
+
+print(listnums[0])
+print(listnums[1])
+print(listnums[2])
+
+"""Las listas pueden contener tanto numeros como cadenas"""
+
+listnums = ["uno", "dos", "tres"]
+intnums = [1,2,3,4,5,6]
+
+"""Tambien podemos crear listas anidadas para representar cuadriculas 2d, como las matrices"""
+
+m = [
+    [1, 2, 3],
+    [4, 5, 6]
+    ]
+    
+print(m[1][2])
+
+"""Las cadenas tambien pueden indexarse como listas"""
+
+str = "Estoy aprendiendo python!!"
+print(str[0])
+print(str[1])
+print(str[2])
+print(str[3])
+print(str[4])
+
+
