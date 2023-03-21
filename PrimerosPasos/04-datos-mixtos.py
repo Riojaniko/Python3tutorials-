6afbb9d1332ddcecc1b76746410c02dfb17968bb
Riojaniko <128044247+Riojaniko@users.noreply.github.com>
2023-03-21 14:29:11 +0100

    Add files via upload

---
+++ PrimerosPasos/04-datos-mixtos.py
@@ -0,0 +1,14 @@
+"""Cada instruccion 'print' imprime el texto en una nueva linea"""
+
+print("hola")
+print("que tal!!!")
+print(1000)
+print(2.2)
+
+"""Una linea de codigo se denomina declaracion, podemos usar tantas declaraciones como nuestro codigo necesite, estas se ejecutaran linea a linea"""
+
+"""El comando 'print' puede tomar multiples valores siempre que sean separados por una coma"""
+
+print("balance:", 5000)
+print("Tengo:", 40, "años!!")
+print("hola", "mundo", "!!", 2023)
